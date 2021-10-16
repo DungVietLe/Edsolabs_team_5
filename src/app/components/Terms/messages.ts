246d6e2837265cd795bc789cf68b4ded8c55f01e
LeeeMoy <dungvietle1299@gmail.com>
2021-10-17 04:06:22 +0700

    fix

---
+++ src/app/components/Terms/messages.ts
@@ -0,0 +1,21 @@
+import { translations } from 'locales/translations';
+import { _t } from 'utils/messages';
+
+export const messages = {
+  title: () => _t(translations.term.title),
+  subTitle: () => _t(translations.term.subTitle),
+  sub1: () => _t(translations.term.sub1),
+  sub2: () => _t(translations.term.sub2),
+  sub3: () => _t(translations.term.sub3),
+  sub4: () => _t(translations.term.sub4),
+  sub5: () => _t(translations.term.sub5),
+  sub6: () => _t(translations.term.sub6),
+  sub7: () => _t(translations.term.sub7),
+  sub8: () => _t(translations.term.sub8),
+  sub9: () => _t(translations.term.sub9),
+  sub10: () => _t(translations.term.sub10),
+  sub11: () => _t(translations.term.sub11),
+  sub12: () => _t(translations.term.sub12),
+  accept: () => _t(translations.term.accept),
+  decline: () => _t(translations.term.decline),
+};
